070ebfe18c28ead9c4a9094865220684fc9f2a22
giapchicuong <giapchicuong@gmail.com>
2024-11-14 17:33:06 +0700

    feat: call api zalo payment

--- lib/screens/update_vip_detail/update_vip_detail_screen.dart
+++ lib/screens/update_vip_detail/update_vip_detail_screen.dart
@@ -1,11 +1,15 @@
+import 'dart:io';
+
 import 'package:do_an_tot_nghiep/features/user/bloc/user_payment_method_get_bloc.dart';
 import 'package:do_an_tot_nghiep/features/user/bloc/user_payment_method_get_event.dart';
 import 'package:do_an_tot_nghiep/features/user/bloc/user_payment_method_get_state.dart';
 import 'package:do_an_tot_nghiep/features/user/data/user_api_client.dart';
 import 'package:do_an_tot_nghiep/features/user/data/user_repository.dart';
+import 'package:do_an_tot_nghiep/screens/update_vip_detail/repo/payment.dart';
 import 'package:do_an_tot_nghiep/utils/formatters/formatter.dart';
 import 'package:do_an_tot_nghiep/utils/theme/theme_ext.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../common/widgets/custom_shapes/containers/arrow_back.dart';
@@ -51,9 +55,11 @@ class UpdateVipDetailScreen extends StatelessWidget {
               const Spacer(),
             ],
           ),
-          body: const Padding(
-            padding: EdgeInsets.all(20.0),
-            child: UpdateVipBody(),
+          body: SingleChildScrollView(
+            child: const Padding(
+              padding: EdgeInsets.all(20.0),
+              child: UpdateVipBody(),
+            ),
           )),
     );
   }
@@ -69,6 +75,125 @@ class UpdateVipBody extends StatefulWidget {
 }
 
 class _UpdateVipBodyState extends State<UpdateVipBody> {
+  static const EventChannel eventChannel =
+      EventChannel('flutter.native/eventPayOrder');
+  static const MethodChannel platform =
+      MethodChannel('flutter.native/channelPayOrder');
+  final textStyle = TextStyle(color: Colors.black54);
+  final valueStyle = TextStyle(
+      color: Colors.orangeAccent, fontSize: 18.0, fontWeight: FontWeight.w400);
+  String zpTransToken = "";
+  String payResult = "";
+  String payAmount = "10000";
+  bool showResult = false;
+  @override
+  void initState() {
+    super.initState();
+    if (Platform.isIOS) {
+      eventChannel.receiveBroadcastStream().listen(_onEvent, onError: _onError);
+    }
+  }
+
+  void _onEvent(dynamic event) {
+    print("_onEvent: '$event'.");
+
+    if (event is Map<String, dynamic>) {
+      setState(() {
+        if (event["errorCode"] == 1) {
+          payResult = "Thanh toán thành công";
+        } else if (event["errorCode"] == 4) {
+          payResult = "User hủy thanh toán";
+        } else {
+          payResult = "Giao dịch thất bại";
+        }
+      });
+    } else {
+      print("Event is not a Map<String, dynamic>: $event");
+    }
+  }
+
+  void _onError(Object error) {
+    print("_onError: '$error'.");
+    setState(() {
+      payResult = "Giao dịch thất bại";
+    });
+  }
+
+  // Button Create Order
+  Widget _btnCreateOrder(String value) => Padding(
+        padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 20.0),
+        child: GestureDetector(
+          onTap: () async {
+            int amount = int.parse(value);
+            if (amount < 1000 || amount > 1000000) {
+              setState(() {
+                zpTransToken = "Invalid Amount";
+              });
+            } else {
+              showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  });
+              var result = await createOrder(amount);
+              if (result != null) {
+                Navigator.pop(context);
+                zpTransToken = result.zptranstoken;
+                setState(() {
+                  zpTransToken = result.zptranstoken;
+                  showResult = true;
+                });
+              }
+            }
+          },
+          child: Container(
+              height: 50.0,
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                color: Colors.purple,
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+              child: Text("Create Order",
+                  style: TextStyle(color: Colors.white, fontSize: 20.0))),
+        ),
+      );
+
+  /// Build Button Pay
+  Widget _btnPay(String zpToken) => Padding(
+      padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 20.0),
+      child: Visibility(
+        visible: showResult,
+        child: GestureDetector(
+          onTap: () async {
+            String response = "";
+            try {
+              final String result =
+                  await platform.invokeMethod('payOrder', {"zptoken": zpToken});
+              response = result;
+              print("payOrder Result: '$result'.");
+            } on PlatformException catch (e) {
+              print("Failed to Invoke: '${e.message}'.");
+              response = "Thanh toán thất bại";
+            }
+            print(response);
+            setState(() {
+              payResult = response;
+            });
+          },
+          child: Container(
+              height: 50.0,
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                color: Colors.pink,
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+              child: Text("Pay",
+                  style: TextStyle(color: Colors.white, fontSize: 20.0))),
+        ),
+      ));
+
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<UserPaymentMethodGetBloc, UserPaymentMethodGetState>(
@@ -105,6 +230,58 @@ class _UpdateVipBodyState extends State<UpdateVipBody> {
                 child: ElevatedButton(
                     onPressed: () {}, child: const Text('Thanh toán')),
               ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  _quickConfig,
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      hintText: 'Amount',
+                      icon: Icon(Icons.attach_money),
+                    ),
+                    initialValue: payAmount,
+                    onChanged: (value) {
+                      setState(() {
+                        payAmount = value;
+                      });
+                    },
+                    keyboardType: TextInputType.number,
+                  ),
+                  _btnCreateOrder(payAmount),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 5.0),
+                    child: Visibility(
+                      child: Text(
+                        "zptranstoken:",
+                        style: textStyle,
+                      ),
+                      visible: showResult,
+                    ),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.symmetric(vertical: 5.0),
+                    child: Text(
+                      zpTransToken,
+                      style: valueStyle,
+                    ),
+                  ),
+                  _btnPay(zpTransToken),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 5.0),
+                    child: Visibility(
+                        child: Text("Transaction status:", style: textStyle),
+                        visible: showResult),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.symmetric(vertical: 5.0),
+                    child: Text(
+                      payResult,
+                      style: valueStyle,
+                    ),
+                  ),
+                ],
+              )
             ],
           );
         }
@@ -115,4 +292,24 @@ class _UpdateVipBodyState extends State<UpdateVipBody> {
       },
     );
   }
+
+  Widget _quickConfig = Container(
+      margin: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                padding: const EdgeInsets.symmetric(vertical: 10.0),
+                child: Text("AppID: 2554"),
+              ),
+            ],
+          ),
+          // _btnQuickEdit,
+        ],
+      ));
 }
